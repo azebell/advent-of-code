03b7d22b754d642ea88acb6600ad246a1d3a3ba5
azebell <averyzebell@gmail.com>
2023-12-07 10:44:20 -0500

    Solve using quadratic formula

--- python/2023/day06.py
+++ python/2023/day06.py
@@ -17,18 +17,18 @@ def part1(input_str: str) -> str:
     return str(answer)
 
 
+def quad_solve(a, b, c):
+    p = -b + (b**2 - 4*a*c)**0.5 / 2*a
+    q = -b - (b**2 - 4*a*c)**0.5 / 2*a
+    return p, q
+
 def part2(input_str: str) -> str:
     lines = input_str.strip().split("\n")
     time = int("".join(lines[0].split()[1:]))
     distance = int("".join(lines[1].split()[1:]))
-    wins = 0
-    for held in range(1, time):
-        travel_time = time - held
-        d = travel_time * held
-        if d > distance:
-            wins += 1
-    return str(wins)
-
+    a, b, c = -1, time, -distance
+    l, r = quad_solve(a, b, c)
+    return str(int(r) - int(l))
 
 if __name__ == "__main__":
     result = run_solver(part1, part2)
