e81f4ed47aed1fa51d592b76b9afe0db299cd19b
Zebell, Avery <Avery.Zebell@gtri.gatech.edu>
2023-12-11 16:23:49 -0500

    Solve 2023 day 9

--- python/2023/day09.py
+++ python/2023/day09.py
@@ -1,14 +1,32 @@
 from aoc import run_solver
 
 
+def solve(line, solver):
+    terms = list(map(int, line.split()))
+    return solver(terms)
+
+def forwards(diffs):
+    if any(diff != 0 for diff in diffs):
+        new_diffs = [y - x for x, y in zip(diffs[:-1], diffs[1:])]
+        return diffs[-1] + forwards(new_diffs)
+    return 0
+
 def part1(input_str: str) -> str:
     lines = input_str.strip().split("\n")
-    return str("answer")
+    answer = sum(solve(line, forwards) for line in lines)
+    return str(answer)
 
+
+def backwards(diffs):
+    if any(diff != 0 for diff in diffs):
+        new_diffs = [y - x for x, y in zip(diffs[:-1], diffs[1:])]
+        return diffs[0] - backwards(new_diffs)
+    return 0
 
 def part2(input_str: str) -> str:
     lines = input_str.strip().split("\n")
-    return str("answer")
+    answer = sum(solve(line, backwards) for line in lines)
+    return str(answer)
 
 
 if __name__ == "__main__":
