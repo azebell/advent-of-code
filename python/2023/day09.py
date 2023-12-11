ff46fd7763b05228b7d91f5ca0f4622e83d0dc70
Avery Zebell <averyzebell@gmail.com>
2023-12-10 23:49:27 -0500

    WIP

---
+++ python/2023/day09.py
@@ -0,0 +1,16 @@
+from aoc import run_solver
+
+
+def part1(input_str: str) -> str:
+    lines = input_str.strip().split("\n")
+    return str("answer")
+
+
+def part2(input_str: str) -> str:
+    lines = input_str.strip().split("\n")
+    return str("answer")
+
+
+if __name__ == "__main__":
+    result = run_solver(part1, part2)
+    print(result)
