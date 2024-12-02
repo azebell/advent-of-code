53ffbeae4962825008fa9e5e94ff919e8f66cbbf
Avery Zebell <averyzebell@gmail.com>
2024-12-02 11:10:52 -0500

    Add boilerplate

---
+++ python/2024/day03.py
@@ -0,0 +1,20 @@
+from aoc import run_solver
+from functools import reduce
+import re
+
+
+def part1(input_str: str) -> str:
+    lines = input_str.strip().split("\n")
+    answer = "unsolved"
+    for line in lines:
+        continue
+    return str(answer)
+
+
+def part2(input_str: str) -> str:
+    return str("unsolved")
+
+
+if __name__ == "__main__":
+    result = run_solver(part1, part2)
+    print(result)
