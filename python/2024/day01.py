25868c63e402ef0e97c2a9182a9cdf3d1e3c812c
Zebell, Avery <Avery.Zebell@gtri.gatech.edu>
2024-12-03 10:23:56 -0500

    Improve 2024 day 1 part 2 from O(n^2) to O(n)

--- python/2024/day01.py
+++ python/2024/day01.py
@@ -18,14 +18,14 @@ def part1(input_str: str) -> str:
 def part2(input_str: str) -> str:
     lines = input_str.strip().split("\n")
     left = []
-    right = []
+    right = dict()
     for line in lines:
         l, r = map(int, line.split())
         left.append(l)
-        right.append(r)
+        right[r] = right.get(r, 0) + 1
     answer = 0
     for num in left:
-        sim = num * right.count(num)
+        sim = num * right.get(num, 0)
         answer += sim
     return str(answer)
 
