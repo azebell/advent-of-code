6f53f1f73a54fa344edaca44b618c1995e9fe9f6
Avery Zebell <averyzebell@gmail.com>
2024-12-05 00:25:42 -0500

    Solve 2024 day 5

---
+++ python/2024/day05.py
@@ -0,0 +1,61 @@
+from aoc import run_solver
+import re
+from functools import reduce
+
+
+def is_correct(update, rules):
+    for i in reversed(range(len(update))):
+        for j in reversed(range(i)):
+            if update[j] in rules.get(update[i], []):
+                return False
+    return True
+
+def part1(input_str: str) -> str:
+    rules, updates = input_str.strip().split("\n\n")
+    rules = [rule.split("|") for rule in rules.strip().split("\n")]
+    updates = [update.split(",") for update in updates.strip().split("\n")]
+
+    before = dict()
+    for prev, next in rules:
+        l = before.get(prev, [])
+        l.append(next)
+        before[prev] = l
+
+    answer = 0
+    for update in updates:
+        if is_correct(update, before):
+            middle = update[(len(update) - 1)//2]
+            answer += int(middle)
+    return str(answer)
+
+def bubble_swap(update, rules):
+    for i in reversed(range(len(update))):
+        for j in reversed(range(i)):
+            if update[j] in rules.get(update[i], []):
+                update[i], update[j] = update[j], update[i]
+    return update
+
+def part2(input_str: str) -> str:
+    rules, updates = input_str.strip().split("\n\n")
+    rules = [rule.split("|") for rule in rules.strip().split("\n")]
+    updates = [update.split(",") for update in updates.strip().split("\n")]
+
+    before = dict()
+    for prev, next in rules:
+        l = before.get(prev, [])
+        l.append(next)
+        before[prev] = l
+
+    answer = 0
+    for update in updates:
+        if is_correct(update, before):
+            continue
+        update = bubble_swap(update, before)
+        middle = update[(len(update) - 1)//2]
+        answer += int(middle)
+    return str(answer)
+
+
+if __name__ == "__main__":
+    result = run_solver(part1, part2)
+    print(result)
